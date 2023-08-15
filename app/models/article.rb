e95d443946f73440a4de0a75369722d23ef84cdb
Maksym Nenashev <57881489+maks-nenashev@users.noreply.github.com>
2023-08-15 23:07:33 +0200

    work version

---
+++ app/models/article.rb
@@ -0,0 +1,12 @@
+class Article < ApplicationRecord
+    mount_uploader :avatar, AvatarUploader # Zagruzka Fotak
+    
+    validates :title, presence: true, length: {minimum: 2} #walidacija
+    validates :text, presence: true#, length: {minimum: 2}
+
+    has_many :comments  # Podkluchenie "Comments" Rukami delaem
+                                                 # has_many :comments, dependent: :nullify
+    has_many :comments, dependent:  :delete_all   # :destroy ---=Dla udalenija Comentarija wmeste s Article
+
+
+end

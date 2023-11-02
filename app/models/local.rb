54e97fa2401c012c5d5c576d859cc3908028edc6
Maksym Nenashev <57881489+maks-nenashev@users.noreply.github.com>
2023-11-02 21:09:57 +0100

    local work

--- app/models/local.rb
+++ app/models/local.rb
@@ -1,3 +1,3 @@
 class Local < ApplicationRecord
-   # has_many :articles
+   has_many :articles
 end
--- db/migrate/20231009165557_create_locals.rb
+++ db/migrate/20231009165557_create_locals.rb
@@ -2,15 +2,16 @@ class CreateLocals < ActiveRecord::Migration[7.0]
   def change
     create_table :locals do |t|
     
-      t.text :title
+      t.string :title
 
       t.timestamps
-    end
-
+     end
+    
          Local.create :title => "Киевская"
          Local.create :title => "Сумская"
          Local.create :title => "Харьковская"
          Local.create :title => "Кировоградская"
          Local.create :title => "Черниговская"
+  
   end
 end

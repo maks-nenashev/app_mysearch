7de5ecaef46fc8c7c8a4e3534174047194a457e1
Maksym Nenashev <57881489+maks-nenashev@users.noreply.github.com>
2024-02-01 19:51:10 +0100

    Changed variables

--- app/models/article.rb
+++ app/models/article.rb
@@ -11,7 +11,7 @@ class Article < ApplicationRecord
     belongs_to :local
     
     def self.ransackable_attributes(auth_object = nil)
-      ["avatar", "created_at", "id", "local_id", "text", "title", "updated_at"]
+      ["image", "created_at", "id", "local_id", "text", "title", "updated_at"]
       end
     def self.ransackable_associations(auth_object = nil)
       ["comments", "local"]
